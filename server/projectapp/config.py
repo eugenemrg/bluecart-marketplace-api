47910928aeafabb014881a8361bc1ef0bd224c53
KenCorp <kennethwarui35@gmail.com>
2023-10-28 20:21:28 +0300

    handling circular imports

--- server/projectapp/config.py
+++ server/projectapp/config.py
@@ -2,17 +2,19 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
+# from flask_restful import Api
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
+app.config['JWT_SECRET_KEY'] = 'super-secret' 
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=30)
 db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 bcrypt = Bcrypt(app)
 
-api = Api(app)
