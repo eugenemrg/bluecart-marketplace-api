de6d8a370a3517b481f9e23be532e2cb46e091c3
KenCorp <kennethwarui35@gmail.com>
2023-10-29 10:29:04 +0300

    intergrating external api

--- server/projectapp/config.py
+++ server/projectapp/config.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
