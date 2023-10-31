d806944ed03dc7375790cb9db9a5e912d3ad87c1
KenCorp <kennethwarui35@gmail.com>
2023-10-31 09:48:10 +0300

    creating description endpoint

--- server/projectapp/app.py
+++ server/projectapp/app.py
@@ -9,6 +9,22 @@ api = Api(app)
 
 
 
+# url = "https://real-time-product-search.p.rapidapi.com/search"
+
+# querystring = {"q":"Nike shoes","country":"us","language":"en"}
+
+# headers = {
+# 	"X-RapidAPI-Key": "355db28ab7msh93f4cc83a76dbbcp154e0cjsn5279bcdc45dc",
+#     # 'x-rapidapi-key': os.environ.get("X_RAPIDAPI_KEY"),
+# 	"X-RapidAPI-Host": "real-time-product-search.p.rapidapi.com"
+#     # 'x-rapidapi-host': os.environ.get("X_RAPIDAPI_HOST_500_MO")
+
+# }
+
+# response = requests.get(url, headers=headers, params=querystring)
+
+# print(response.json())
+
 
 @app.route("/", methods=["GET"])
 def get_products():
@@ -16,8 +32,9 @@ def get_products():
     
     search_term = request.args.get('search_term', '').replace(' ', '+')
     query = request.args.get("q")
-    # querystring = {"q": query, "search_text": search_term}
-    querystring = {"q":search_term,"country":"us","language":"en"}
+    # querystring = {"q": search_term}
+    # description = responce.json()['data'][0]['product_description']
+    querystring = {"q":"iphone","country":"us","language":"en"}
 
 
     headers = {
@@ -27,13 +44,57 @@ def get_products():
 
     r = requests.get(url, headers=headers, params=querystring)
     
-    if r.status_code == 200:
-        # print (r.json())
-        return jsonify(r.json())
+    data = r.json().get('data', [])
+    if data:
+            # description = responce.json()['data'][0]['product_description']
+            description = data[0].get('product_description')
+            return jsonify({"descrition": description})
     else:
-        return jsonify({"error":"Request failed"})
+         return jsonify({"am dead":"six months wasted"})
+    # if r.status_code == 200:
+    #     # print (r.json())
+    #     return jsonify(r.json())
+    # else:
+    #     return jsonify({"error":"Request failed"})
     
 time.sleep(60)
+
+
+# url = "https://amazon23.p.rapidapi.com/product-search"
+
+# querystring = {"query":"xbox","country":"US"}
+
+# headers = {
+# 	"X-RapidAPI-Key": "355db28ab7msh93f4cc83a76dbbcp154e0cjsn5279bcdc45dc",
+# 	"X-RapidAPI-Host": "amazon23.p.rapidapi.com"
+# }
+
+# response = requests.get(url, headers=headers, params=querystring)
+
+# print(response.json())
+
+# class Amazon_api(Resource):
+#     def get(self):
+#         url = "https://amazon23.p.rapidapi.com/product-search"
+#         search_term = request.args.get('search_term', '').replace(' ', '+')
+#         querystring = {"q": search_term}
+
+#         headers = {
+#             "X-RapidAPI-Key": "355db28ab7msh93f4cc83a76dbbcp154e0cjsn5279bcdc45dc",
+#             "X-RapidAPI-Host": "amazon23.p.rapidapi.com"
+#         }
+        
+#         r = requests.get(url, headers=headers, params=querystring)
+        
+#         if r.status_code == 200:
+#             return jsonify(r.json())
+#         else:
+#             return jsonify({"error": "Request failed"})
+
+# api.add_resource(Amazon_api, "/products")
+
+
+
 
 class GetUsers(Resource):
     def get(self):
