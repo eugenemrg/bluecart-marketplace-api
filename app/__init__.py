07500fe80dc3a8862251dd79af4fd0cbb3d4eccc
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-11-04 10:47:30 +0300

    Added and initialized Flask JWT extended

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from .extensions import api, db, migrate, bcrypt
+from .extensions import api, db, migrate, bcrypt, jwt
 from .routes import profile_ns, login_ns, history_ns
 
 def create_app():
     app = Flask(__name__)
     
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config["JWT_SECRET_KEY"] = "phase5JWTkey"
     
     api.init_app(app)
     db.init_app(app)
     bcrypt.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
     
     api.add_namespace(profile_ns)
     api.add_namespace(login_ns)
