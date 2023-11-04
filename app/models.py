3da5eecba1c2656a9879d936758b9b5988df4db6
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-11-04 11:45:49 +0300

    Updated models, added missing password property on User model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique = True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String)
 
     search_history = db.relationship('SearchHistory', backref='user')
 
